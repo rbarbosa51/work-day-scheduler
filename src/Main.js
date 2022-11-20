71d1eefa5b0a2f856817b35e3a110935e5029bba
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-20 15:25:07 -0600

    In this commit: Figured out the Fontawesome problem: 1) I needed to install the icons themselves npm install @fortawesome/free-solid-svg-icons 2) I needed to import them one I was using import { faSave } from "@fortawesome/free-solid-svg-icons"; 3) Then instead of the i tag with the fontawesome class: <i class="fas fa-save" aria-hidden="true"></i> Now I had to use the react-fontawesome react component <FontAwesomeIcon icon={faSave} /> 4) THIS WAS KIND OF HARD TO FIGURE OUT!!!

--- src/Main.js
+++ src/Main.js
@@ -1,4 +1,5 @@
-//
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSave } from "@fortawesome/free-solid-svg-icons";
 
 function Main() {
     return (
@@ -8,7 +9,7 @@ function Main() {
                 <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
                     <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
                     <button class="btn saveBtn col-2 col-md-1" aria-label="save">
-                        <i class="fas fa-save" aria-hidden="true"></i>
+                        <FontAwesomeIcon icon={faSave} />
                     </button>
                 </div>
             </div>
