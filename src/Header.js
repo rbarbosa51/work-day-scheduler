309efa011811f4c6fcc97293991d563289ed0dcf
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-20 15:00:51 -0600

    In this commit: I managed to recreate the original boilerplate header.

--- src/Header.js
+++ src/Header.js
@@ -2,8 +2,11 @@
 
 function Header() {
     return (
-        <header>
-            <p>This is a test. The Quick brown fox jumped over the lazy dog.</p>
+        <header className="p-5 mb-4 border-5 border-bottom border-dark text-center">
+            <h1 className="display-3">Work Day Scheduler</h1>
+            <p className="lead">A simple calendar app for scheduling your work day</p>
+            {/* This will be the reactive day.js component */}
+            <p id="currentDay" className="lead"></p>
         </header>
     );
 }
