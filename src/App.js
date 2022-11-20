5be52b2bd29e9aaa3a58fb71841013fe412c10ac
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-20 14:06:29 -0600

    In this commit: (Experiment Branch) I deleted the index.css file produced by create-react-app. In its place, I placed the style.css. I copied and pasted the contents of the Module's 5 Challenge styles.css into this style.css. I also used the import statement on the index.js file. Deleted the App.css and index.css from the Directory. I will now merge this branch with main. I will then delete this Experiment branch.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+//import './style.css';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.css';
 import App from './App';
 
 
