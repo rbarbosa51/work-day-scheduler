8922fb905b9ac9b5ca780267656fe61d8d4cb01a
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-20 15:47:34 -0600

    Attention to TA's, or Graders: In this commit: I fully recreate the appearance of the original Module 5 Challenge boiler plate code. It still does not do anything. It only shows 9am, 10am, and 11am exactly as the original.

--- src/Main.js
+++ src/Main.js
@@ -5,13 +5,31 @@ function Main() {
     return (
         <main>
             <div className="container-lg px-5">
-            <div id="hour-9" class="row time-block past">
-                <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+
+                <div id="hour-9" class="row time-block past">
+                    <div class="col-2 col-md-1 hour text-center py-3">9AM</div>
                     <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
                     <button class="btn saveBtn col-2 col-md-1" aria-label="save">
                         <FontAwesomeIcon icon={faSave} />
                     </button>
                 </div>
+
+                <div id="hour-10" class="row time-block present">
+                    <div class="col-2 col-md-1 hour text-center py-3">10AM</div>
+                    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+                    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+                        <FontAwesomeIcon icon={faSave} />
+                    </button>
+                </div>
+
+                <div id="hour-11" class="row time-block future">
+                    <div class="col-2 col-md-1 hour text-center py-3">11AM</div>
+                    <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
+                    <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+                        <FontAwesomeIcon icon={faSave} />
+                    </button>
+                </div>
+
             </div>
         </main>
     );
