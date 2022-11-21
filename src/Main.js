993cea6563de0e44119efcf1374fe6474101f22c
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-21 13:15:37 -0600

    In this commit: Made a new array inside the Main component called hourSpecs. It contains all of the hour and time blocks for all of the rows. Then I use the Javascript array map function to map the results to Hour components.

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,18 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSave } from "@fortawesome/free-solid-svg-icons";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { faSave } from "@fortawesome/free-solid-svg-icons";
 import Hour from "./Hour";
 
 function Main() {
-    
+    const hourSpecs = [
+        {hour: '9AM', timeBlock: 'past'},
+        {hour: '10AM', timeBlock: 'present'},
+        {hour: '11AM', timeBlock: 'future'}
+    ];
     return (
         <main className="container-lg px-5">
-            
-                <Hour hour='9AM' timeBlock='past' />
-                <Hour hour='10AM' timeBlock='present' />
-                <Hour hour='11AM' timeBlock='future' />
-
+            {hourSpecs.map( (p) => {
+                return <Hour hour={p.hour} timeBlock={p.timeBlock} />
+            })}
         </main>
     );
 }
