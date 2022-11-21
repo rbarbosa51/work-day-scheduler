586a5cf428afce61152c3e80825b46abd5bf905e
Rafael Barbosa <rafael.barbosa51@gmail.com>
2022-11-21 15:59:17 -0600

    In this commit (Experiment 4 Branch) I ised npm install dayjs to install the dayjs library I added a useState hook to the header and used it to get the dayjs time and set it to the p tag.

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,16 @@
-//import
+import { useState } from "react";
+import dayjs from "dayjs";
 
 function Header() {
+    const [time] = useState( () => {
+        return dayjs().format('dddd, MMMM D YYYY');
+    });
     return (
         <header className="p-5 mb-4 border-5 border-bottom border-dark text-center">
             <h1 className="display-3">Work Day Scheduler</h1>
             <p className="lead">A simple calendar app for scheduling your work day</p>
             {/* This will be the reactive day.js component */}
-            <p id="currentDay" className="lead"></p>
+            <p className="lead">{time}</p>
         </header>
     );
 }
